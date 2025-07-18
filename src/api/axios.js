745ea463036f0d76966e41d2a5724b1d71a292b0
marieglennalano <marieglennalano@gmail.com>
2025-07-18 11:23:31 +0800

    Fixed Code

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,5 @@
 import axios from 'axios';
 
-const instance = axios.create({
-  baseURL: 'http://localhost:4000',
-  headers: {
-    'Content-Type': 'application/json'
-  }
+export default axios.create({
+  baseURL: process.env.REACT_APP_API_URL // Make sure this matches .env key
 });
-
-export default instance;
